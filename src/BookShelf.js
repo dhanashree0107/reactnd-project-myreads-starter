043867517655fc9f010f2819e1220579ef3135ac
Dhanashree0107 <dhanashreekere17@gmail.com>
2020-05-23 14:21:12 +0530

    created components that holds data

---
+++ src/BookShelf.js
@@ -0,0 +1,37 @@
+import React,{Component} from 'react'
+import SingleShelf from './SingleShelf'
+
+class BookShelf extends Component{
+    render(){
+        return(
+        <div>
+
+            <div>
+        <div className="bookshelf">
+        <h2 className="bookshelf-title">Currently Reading</h2>
+        <SingleShelf type='currentlyReadng' Books={this.props.books.filter((book) => 
+            book.shelf==='currentlyReading')}/>
+        </div>
+        </div>
+
+        <div>
+        <div className="bookshelf">
+        <h2 className="bookshelf-title">Want to Read</h2>
+        <SingleShelf type='wantToRead' Books={this.props.books.filter((book) => 
+            book.shelf==='wantToRead')}/>
+        </div>
+        </div>
+
+        <div>
+        <div className="bookshelf">
+        <h2 className="bookshelf-title">Read</h2>
+        <SingleShelf type='read' Books={this.props.books.filter((book) => 
+            book.shelf==='read')}/>
+        </div>
+        </div>
+        </div>
+        )
+    }
+}
+
+export default BookShelf
---
+++ src/SingleShelf.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+import Book from './Book';
+
+
+class SingleShelf extends Component{
+      render(){
+          const {books} = this.props
+          return(
+          <div className="bookshelf-books">
+          <ol className="books-grid">
+              {
+                  books.map((book) =>
+                  (<Book key={book.id} book={book}/>))
+              }
+              </ol>
+              </div>
+              );
+      }
+}
+
+export default SingleShelf
