c9c2b1d1eca9a4b382c0b5361abf4cf163ce1293
Dhanashree0107 <dhanashreekere17@gmail.com>
2020-05-23 20:29:43 +0530

    search books using booksAPI , simplified hard codes into Components

--- src/App.js
+++ src/App.js
@@ -1,92 +1,45 @@
 import React from 'react'
-//import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
-import Book from './Book'
+//import Book from './Book'
+import BookShelf from './BookShelf'
+import Search from './Search'
+import { Router } from 'react-router-dom'
+import {Route, Link} from 'react-router-dom'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    
+    books :[]
   }
+ 
+   componentDidMount(){
+       BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
+   
+   }
 
   render() {
+    console.log(this.state.books)
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
+        
+         <Route path='/' component={Search}/>
+          <Route path='/' render={() =>(
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
             </div>
             <div className="list-books-content">
-              <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <li>
-                        <Book />
-                      </li>
-                     
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <li>
-                      <Book />
-                      </li>
-                    
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <li>
-                      <Book />
-                      </li>
-                    
-                     
-                    </ol>
-                  </div>
-                </div>
-              </div>
+              <BookShelf />
             </div>
             <div className="open-search">
               <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
             </div>
           </div>
-        )}
-      </div>
+          )}/>
+        </div>
     )
   }
 }
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,34 +1,23 @@
 import React,{Component} from 'react'
-import SingleShelf from './SingleShelf'
+import Book from './Book'
 
 class BookShelf extends Component{
     render(){
         return(
         <div>
 
-            <div>
-        <div className="bookshelf">
-        <h2 className="bookshelf-title">Currently Reading</h2>
-        <SingleShelf Books={this.props.books.filter((book) => 
-            book.shelf==='currentlyReading')}/>
+<div className="bookshelf">
+                  <h2 className="bookshelf-title">Currently Reading</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      <li>
+                        <Book />
+                      </li>
+                     
+                    </ol>
+                  </div>
         </div>
-        </div>
-
-        <div>
-        <div className="bookshelf">
-        <h2 className="bookshelf-title">Want to Read</h2>
-        <SingleShelf Books={this.props.books.filter((book) => 
-            book.shelf==='wantToRead')}/>
-        </div>
-        </div>
-
-        <div>
-        <div className="bookshelf">
-        <h2 className="bookshelf-title">Read</h2>
-        <SingleShelf Books={this.props.books.filter((book) => 
-            book.shelf==='read')}/>
-        </div>
-        </div>
+        
         </div>
         )
     }
